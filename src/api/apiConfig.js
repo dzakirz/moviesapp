d0be17b123675d31642349bec65148a2a10475af
rzdzaky <76608040+rzdzaky@users.noreply.github.com>
2022-02-12 19:02:55 +0700

    deploy

---
+++ src/api/apiConfig.js
@@ -0,0 +1,8 @@
+const apiConfig = {
+    baseUrl: 'https://api.themoviedb.org/3/',
+    apiKey: '2588072e53fb37c608b4b2a6cc38fe9f',
+    originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
+    w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
+}
+
+export default apiConfig;
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import './footer.scss';
+
+import { Link } from 'react-router-dom';
+
+import bg from '../../assets/footer-bg.jpg';
+import logo from '../../assets/tmovie.png';
+
+const Footer = () => {
+    return (
+        <div className="footer" style={{backgroundImage: `url(${bg})`}}>
+            <div className="footer__content container">
+                <div className="footer__content__logo">
+                    <div className="logo">
+                        <Link to="/">Movies App</Link>
+                    </div>
+                </div>
+                <div className="footer__content__menus">
+                    <div className="footer__content__menu">
+                        <Link to="/">Home</Link>
+                        <a href='https://www.instagram.com/rzdzaky/' target='_blank'>Contact us</a>
+                        <a href='https://www.instagram.com/rzdzaky/' target='_blank'>Term of services</a>
+                        <a href='https://www.instagram.com/rzdzaky/' target='_blank'>About us</a>
+                    </div>
+                    <div className="footer__content__menu">
+                        <a href='https://www.instagram.com/rzdzaky/' target='_blank'>Live</a>
+                        <a href='https://www.instagram.com/rzdzaky/' target='_blank'>FAQ</a>
+                        <a href='https://www.instagram.com/rzdzaky/' target='_blank'>Premium</a>
+                        <a href='https://www.instagram.com/rzdzaky/' target='_blank'>Pravacy policy</a>
+                    </div>
+                    <div className="footer__content__menu">
+                        <Link to="/movie">You must watch</Link>
+                        <Link to="/movie">Recent release</Link>
+                        <Link to="/movie">Top IMDB</Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
