17eebe936a13291a3aeb7dd994fef69acd7c9ee8
Dzakirz <dzakirz.dev@gmail.com>
2022-05-12 16:24:46 +0700

    upload

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -3,7 +3,7 @@ dotenv.config()
 
 const apiConfig = {
   baseUrl: "https://api.themoviedb.org/3/",
-  apiKey: "2588072e53fb37c608b4b2a6cc38fe9f",
+  apiKey: process.env.API_KEY,
   originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
   w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
 }
