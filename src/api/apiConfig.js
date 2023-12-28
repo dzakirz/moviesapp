13180fabe14ad7c284c20802f25d82d7a1c837e7
Dzaki Rozaan <dzakirz.dev@gmail.com>
2023-12-28 21:59:02 +0700

    fix api & icon on android pwa

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -3,7 +3,7 @@ dotenv.config()
 
 const apiConfig = {
   baseUrl: "https://api.themoviedb.org/3/",
-  apiKey: process.env.API_KEY,
+  apiKey: process.env.REACT_APP_API_KEY,
   originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
   w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
 }
